4e34988a873c8607f69f4f0451af41c365e0abbc
jungchanSon <rnrmfjc@gmail.com>
2025-01-30 20:02:50 +0900

    feat: add spring gateway

---
+++ infra-gateway/src/main/java/com/kiosk/server/config/Route.java
@@ -0,0 +1,21 @@
+package com.kiosk.server.config;
+
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Route {
+
+    @Bean
+    public RouteLocator cRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(
+                        r -> r.path("/user/**")
+                                .uri("http://user-service-app:8090")
+                ).build();
+
+    }
+}
